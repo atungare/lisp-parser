007a30c84940da3f003a352f114c2dc5238b840e
atungare <ajay.tungare@gmail.com>
2016-12-16 14:35:58 -0800

    working ast parser

--- parser.js
+++ parser.js
@@ -6,13 +6,54 @@ export function tokenize (expression) {
     .split(/\s+/);
 }
 
-export function buildAST (tokens) {
-  return tokens.reduce((ast, token) => {
-
-  }, []);
+function readToken (token) {
+  if (token === '(') {
+    return {
+      type: 'OPEN_PARENS'
+    };
+  } else if (token === ')') {
+    return {
+      type: 'CLOSED_PARENS'
+    };
+  } else if (token.match(/^\d+$/)) {
+    return {
+      type: 'INTEGER',
+      value: parseInt(token)
+    };
+  } else {
+    return {
+      type: 'SYMBOL',
+      value: token
+    };
+  }
 }
 
-export default function parse (expression) {
+export function buildAST (tokens) {
+  return tokens.reduce((ast, t) => {
+    const token = readToken(t);
+
+    if (token.type === 'OPEN_PARENS') {
+      ast.push([]);
+    } else if (token.type === 'CLOSED_PARENS') {
+      const current_sexp = ast.pop();
+      ast[ast.length - 1].push(current_sexp);
+    } else {
+      const current_sexp = ast.pop();
+      current_sexp.push(token.value);
+      ast.push(current_sexp);
+    }
+
+    return ast;
+  }, [[]])[0][0];
+}
+
+
+export function parse (expression) {
   const tokens = tokenize(expression);
   return buildAST(tokens);
+}
+
+
+export function evaluate (ast) {
+  // TODO
 }
--- test/parser.spec.js
+++ test/parser.spec.js
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 
-import parser, { tokenize, buildAST } from '../parser';
+import { tokenize, buildAST, parse, evaluate } from '../parser';
 
 describe('tokenize', () => {
   it('should digest an expression string into a list of tokens', () => {
@@ -14,6 +14,28 @@ describe('buildAST', () => {
   it('should convert a list of tokens into an abstract syntax tree', () => {
     assert.deepEqual(buildAST(['(', '1', '2', '3', ')']), [1, 2, 3]);
     assert.deepEqual(buildAST(['(', 'ay', 'bee', 'cee', ')']), ['ay', 'bee', 'cee']);
+  });
+  it('should work recursively', () => {
     assert.deepEqual(buildAST(['(', '1', '(', '2', '3', ')', ')']), [1, [2, 3]]);
+  });
+});
+
+describe('parse', () => {
+  it('should convert an expression string into an abstract syntax tree', () => {
+    assert.deepEqual(parse('(1 2 3)'), [1, 2, 3]);
+    assert.deepEqual(parse('(+ 2 3)'), ['+', 2, 3]);
+  });
+  it('should work recursively', () => {
+    assert.deepEqual(parse('(+ (+ 1 1) 3)'), ['+', ['+', 1, 1], 3]);
+  });
+});
+
+
+describe('evaluate', () => {
+  it('should evaluate the abstract syntax tree of an expression into a value', () => {
+    assert.equal(evaluate(['+', 2, 3]), 5);
+  });
+  it('should work recursively', () => {
+    assert.equal(evaluate(['+', 2, ['+', 1, 2]]), 5);
   });
 });
